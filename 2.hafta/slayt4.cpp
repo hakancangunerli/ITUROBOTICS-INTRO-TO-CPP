8cdc8e918dbb9aeca0bc7b2bbaf4db1e38972da9
Hakan John Gunerli <hakancangunerli@protonmail.com>
2021-09-18 16:58:25 +0000

    added code from the slides

---
+++ 2.hafta/slayt4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+bool boolValue = false;
+int integerNumber = 1;  
+float floatNumber = 12.5;  
+double doubleNumber = 24.45;
+// void test;  // hic bir sonuc almayacak, slaytta dedigimiz gibi 
+
+std::cout << boolValue << "\n"; 
+std::cout << integerNumber << "\n" ;
+std::cout << floatNumber << "\n" ; 
+std::cout << doubleNumber << "\n"; 
+// std::cout << test; 
+
+    
+
+    return 0;
+}
---
+++ 2.hafta/slayt8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void addition (int a, int b)
+{
+  int r;
+  r=a+b;
+  std::cout << "Sonuc: " << r << "\n";
+}
+
+int main() {
+  
+addition (5,3);
+      return 0;
+}
